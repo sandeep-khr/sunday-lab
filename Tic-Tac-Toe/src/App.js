30a9275148b007dd5386e6630da09a6377980b92
Sandeep <sandeepkharoundha@gmail.com>
2025-08-27 19:21:43 +0530

    Refactor Square component to accept value prop for dynamic rendering

--- Tic-Tac-Toe/src/App.js
+++ Tic-Tac-Toe/src/App.js
@@ -12,10 +12,38 @@
 
 
 
-export default function Square() {
-  // Return a JSX element: a <button> with the class 'square' and the text 'X'.
-  // JSX is a syntax extension that lets you write HTML-like code inside JavaScript.
-  // The 'className' attribute is used instead of 'class' because 'class' is a reserved word in JS.
-  return <button className="square">X</button>;
+// export default function Square() {
+//   // Return a JSX element: a <button> with the class 'square' and the text 'X'.
+//   // JSX is a syntax extension that lets you write HTML-like code inside JavaScript.
+//   // The 'className' attribute is used instead of 'class' because 'class' is a reserved word in JS.
+//   return <button className="square">X</button>;
+// }
+
+import { useState } from "react";
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
